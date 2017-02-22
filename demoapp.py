90717611f25c31c469ffe263b1867fb718c89eb8
devbox <devbox@devbox.localdomain>
2017-02-22 04:00:13 -0500

    Added hello/universe to the application

--- demoapp.py
+++ demoapp.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 
@@ -13,7 +14,13 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/hello/world')
 
+class HelloUniverse(Resource):
+    def get(self):
+        text = "Hello Universe!"
+        return text
+
+api.add_resource(HelloUniverse, '/hello/universe')
+
 if __name__ == '__main__':
-    # Runn Flask
+    # Run Flask
     app.run(debug=True, host='0.0.0.0', port=int("5000"))
-    # pass
